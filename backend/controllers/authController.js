3d5e4951cd235004a6c81da94b4bf282dd6d4165
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-02 01:58:47 +0600

    admin login [added]

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,17 +9,19 @@ exports.login = async (req, res) => {
     try {
         let token = '';
         const { phoneNumber, password } = req.body;
-        const users = await Approved.findOne({ phoneNumber });
         const user = await User.findOne({ phoneNumber });
 
-        if (users && users.blocked) {
-            return res.status(403).json({ message: "User is blocked" });
-        }
+        const kycDone = await Approved.findOne({ phoneNumber });
+
+        // if (users && users.blocked) {
+        //     return res.status(403).json({ message: "User is blocked" });
+        // }
         if (!user) {
             return res.status(401).json({ error: "Invalid credentials" });
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
+        console.log(isPasswordValid)
         if (!isPasswordValid) {
             return res.status(401).json({ error: "Invalid credentials" });
         }
@@ -27,7 +29,7 @@ exports.login = async (req, res) => {
         if(user.role === "Admin"){
             token = jwt.sign({ userId: user._id }, SECRET_KEY, { expiresIn: "24hr" });
         }else{
-            token = jwt.sign({ userId: user._id, nid: users.nid }, SECRET_KEY, { expiresIn: "24hr" });
+            token = jwt.sign({ userId: user._id, nid: kycDone.nid }, SECRET_KEY, { expiresIn: "24hr" });
         }
         res.json({ message: "Login successful", role: user.role, token: token });
     } catch (error) {
