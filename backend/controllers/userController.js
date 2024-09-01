c7e0f00c9d40b75958b313885957845d226bd21b
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-01 19:28:35 +0600

    Already having tin [added backend]

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,3 +26,28 @@ exports.changeContact = async (req, res) => {
         res.status(500).json({ message: 'Error getting or saving data', error });
     }
 };
+
+exports.isHavingTin = async (req, res) => {
+    try {
+        const user = req.user;
+
+        const getFormData = await FormData.findOne({
+            "nid": user.nid,
+        })
+
+        if (!getFormData) {
+            return res.status(404).json({ message: 'Data not found' });
+        }
+
+        if (getFormData.information.tin) {
+            res.status(200).json({ message: 'Yes, user has TIN', 
+                tin: getFormData.information.tin
+             });
+        } else {
+            res.status(202).json({ message: 'No, user does not have TIN' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error getting data', error });
+    }
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
+// POST
 router.post("/change-contact", authMiddleware, userController.changeContact);
+
+// GET
+router.get("/is-having-tin", authMiddleware, userController.isHavingTin);
 
 module.exports = router;
